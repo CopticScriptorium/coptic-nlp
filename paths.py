c7ce734844e90ed7e816e4c6c8cd1a5137acc093
amir-zeldes <amir.zeldes@georgetown.edu>
2017-10-16 17:06:22 -0400

    paths for stacked_tokenizer

--- paths.py
+++ paths.py
@@ -2,6 +2,7 @@ import platform
 
 if platform.system() == 'Linux':
 	tokenizer_path = '' # Path to tokenize_coptic.pl
+	stacked_tok_path = '' # Path to stacked_tokenizer.py
 	tt_path = '' # Path to TreeTagger
 	lang_path = '' # Path to language of origin script
 	milestone_path = '' # Path to binarize_tags.pl
@@ -9,9 +10,11 @@ if platform.system() == 'Linux':
 	conllize_path = '' # Path to TT2CoNLL.pl
 	parser_path = '' # Path to MaltParser
 	depedit_path = '' # Path to depedit.py
+	python3 = 'python3.5'
 else:
 	# Alternate paths for running under Windows or Mac
 	tokenizer_path = ''
+	stacked_tok_path = ''
 	tt_path = ''
 	lang_path = ''
 	milestone_path = ''
@@ -19,3 +22,4 @@ else:
 	conllize_path = ''
 	parser_path = ''
 	depedit_path = ''
+	python3 = 'python3.5'
