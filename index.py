f5d670045d2f152d73833495f1d77c259ba03266
amir-zeldes <amir.zeldes@georgetown.edu>
2019-05-04 10:05:49 -0400

    Simplify import

--- index.py
+++ index.py
@@ -4,7 +4,6 @@
 import cgitb
 cgitb.enable()
 
-from lib.tokenize_rf import MultiColumnLabelEncoder, DataFrameSelector, lambda_underscore
 from nlp_form import make_nlp_form
 
 print("Content-Type: text/html\n\n\n")
