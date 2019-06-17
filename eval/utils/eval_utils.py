c983c17da85cd05b8fbf345b84ac4f53d767b268
amir-zeldes <amir.zeldes@georgetown.edu>
2019-06-17 10:38:53 -0400

    eval utils

---
+++ eval/utils/eval_utils.py
@@ -0,0 +1,166 @@
+import tempfile, subprocess
+import io, os, sys
+from glob import glob
+
+PY3 = sys.version_info[0] == 3
+
+script_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep
+eval_dir = script_dir + ".." + os.sep
+
+
+def exec_via_temp(input_text, command_params, workdir="", outfile=False):
+	temp = tempfile.NamedTemporaryFile(delete=False)
+	if outfile:
+		temp2 = tempfile.NamedTemporaryFile(delete=False)
+	output = ""
+	try:
+		if input_text is not None:
+			temp.write(input_text.encode("utf8"))
+			temp.close()
+
+			if outfile:
+				command_params = [x if 'tempfilename2' not in x else x.replace("tempfilename2",temp2.name) for x in command_params]
+			command_params = [x if 'tempfilename' not in x else x.replace("tempfilename",temp.name) for x in command_params]
+		if workdir == "":
+			proc = subprocess.Popen(command_params, stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+			(stdout, stderr) = proc.communicate()
+		else:
+			proc = subprocess.Popen(command_params, stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE,cwd=workdir)
+			(stdout, stderr) = proc.communicate()
+		if outfile:
+			if PY3:
+				output = io.open(temp2.name,encoding="utf8").read()
+			else:
+				output = open(temp2.name).read()
+			temp2.close()
+			os.remove(temp2.name)
+		else:
+			output = stdout
+		#print(stderr)
+		proc.terminate()
+	except Exception as e:
+		print(e)
+	finally:
+		if PY3:
+			if not outfile:
+				output = output.decode("utf8").replace("\r","")
+		if input_text is not None:
+			os.remove(temp.name)
+		return output
+
+
+def exec_via_temp_old(input_text, command_params, workdir=""):
+	exec_out = ""
+	try:
+		if input_text is not None:
+			temp = tempfile.NamedTemporaryFile(delete=False)
+			if PY3:
+				# try:
+				temp.write(input_text.encode("utf8"))
+				# except:
+				# 	temp.write(input_text)
+			else:
+				temp.write(input_text)
+			temp.close()
+
+			command_params = [x if x != 'tempfilename' else temp.name for x in command_params]
+		if workdir == "":
+			proc = subprocess.Popen(command_params, stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+			(stdout, stderr) = proc.communicate()
+		else:
+			proc = subprocess.Popen(command_params, stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE,cwd=workdir)
+			(stdout, stderr) = proc.communicate()
+
+		exec_out = stdout
+	except Exception as e:
+		print(e)
+	finally:
+		if input_text is not None:
+			os.remove(temp.name)
+		if PY3:
+			try:
+				exec_out = exec_out.decode("utf8").replace("\r","")
+			except:
+				pass
+		return exec_out
+
+
+def list_files(alias="silver",file_dir=None):
+
+	if file_dir is None:
+		file_dir = eval_dir + "tt" + os.sep # Default tt file directory
+
+	file_list = []
+	if alias.lower() == "cyrus":
+		file_dir = eval_dir + "unreleased" + os.sep
+		file_list = [file_dir + "BritMusOriental6783_part1.tt",file_dir + "BritMusOriental6783_part2.tt"]
+	elif alias.lower() == "cyrus_plain":
+		file_dir = eval_dir + "plain" + os.sep
+		file_list = [file_dir + "BritMusOriental6783_part1.txt",file_dir + "BritMusOriental6783_part2.txt"]
+	elif alias.lower() == "ud_test":
+		file_list = io.open(eval_dir + "test_list.tab").read().strip().split("\n")
+		file_list = [file_dir + f for f in file_list]
+	elif alias.lower() == "victor_plain":
+		file_dir = eval_dir + "plain" + os.sep
+		file_list = [file_dir + "martyrdom.victor.txt"]
+	elif alias.lower() == "victor_tt":
+		file_dir = eval_dir + "plain" + os.sep
+		file_list = [file_dir + "martyrdom.victor.01.tt"]
+	elif alias.lower() == "silver":
+		test_list = io.open(eval_dir + "test_list.tab").read().strip().split("\n")
+		file_list = glob(file_dir + os.sep + "*.tt")
+		filtered = []
+		for f in file_list:
+			if os.path.basename(f) not in test_list:
+				filtered.append(f)
+		file_list = filtered
+
+	return file_list
+
+
+def get_col(data, colnum):
+	if not isinstance(data,list):
+		data = data.split("\n")
+
+	splits = [row.split("\t") for row in data if "\t" in row]
+	return [r[colnum] for r in splits]
+
+
+def inject_col(source_lines, target_lines, col=-1, into_col=None, skip_supertoks=False):
+
+	output = []
+	counter = -1
+	target_line = ""
+
+	if not PY3:
+		if isinstance(target_lines,unicode):
+			target_lines = str(target_lines.encode("utf8"))
+
+	if not isinstance(source_lines,list):
+		source_lines = source_lines.split("\n")
+	if not isinstance(target_lines,list):
+		target_lines = target_lines.split("\n")
+
+	for i, source_line in enumerate(source_lines):
+		while len(target_line) == 0:
+			counter +=1
+			target_line = target_lines[counter]
+			if (target_line.startswith("<") and target_line.endswith(">")) or len(target_line) == 0:
+				output.append(target_line)
+				target_line = ""
+			else:
+				target_cols = target_line.split("\t")
+				if "-" in target_cols[0] and skip_supertoks:
+					output.append(target_line)
+					target_line = ""
+		source_cols = source_line.split("\t")
+		to_inject = source_cols[col]
+		target_cols = target_line.split("\t")
+		if into_col is None:
+			target_cols.append(to_inject)
+		else:
+			target_cols[into_col] = to_inject
+		output.append("\t".join(target_cols))
+		target_line=""
+
+	return "\n".join(output)
---
+++ eval/utils/f_score_segs.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import io, sys, argparse
+
+"""
+Script to evaluate segmentation f-score and perfect super-token segmentation proportion from two files:
+ * goldfile: single column file, one super-token perline, pipes denote segmentation positions	
+	* predfile: same format
+	
+Stripped files are assumed to have the same amount of characters except pipes. A super-token may also consist
+of a single pipe (i.e. input contained a literal pipe, which must be a complete super-token).
+"""
+
+
+def main(goldfile, predfile, preds_as_string=False,ignore_diff_len=False,replace_diff_len=False,silent=False):
+	lines = io.open(goldfile, encoding="utf8").read().strip().replace("\r", "").split("\n")
+	counter = 0
+	gold = []
+	gold_groups = []
+	perfect = 0
+	total = 0
+
+	if "\t" in lines[0]:  # Convenience step allowing 2-column file as gold
+		sys.stderr.write("o Found tab in gold file, using second column as gold\n")
+		lines = [line.split("\t")[1] for line in lines if "\t" in line]
+	for r, line in enumerate(lines):
+		total += 1
+		if "[" in line or "]" in line and not len(line.strip()) == 1:
+			line = line.replace("[","").replace("]","")
+		gold_groups.append(line.strip())
+		for i, c in enumerate(list(line.strip())):
+			counter += 1
+			if i == len(line.strip()) - 1:  # Last character is trivial, ignore
+				continue
+			if c == "|":
+				if len(line.strip()) > 1 and i == 0:
+					print("Complex token begins with boundary marker at gold row " + str(r))
+					sys.exit()
+				counter -=1
+				gold[-1] = 1
+			else:
+				gold.append(0)
+
+	gold_chars = counter
+
+	if preds_as_string:
+		lines = predfile.split("\n")
+	else:
+		lines = io.open(predfile, encoding="utf8").read().strip().replace("\r", "").split("\n")
+	counter = 0
+	pred = []
+
+	to_pop = []
+	for r, line in enumerate(lines):
+		# Ignore [ or ] in string
+		if "[" in line or "]" in line and not len(line.strip()) == 1:
+			line = line.replace("[","").replace("]","")
+		if r == len(gold_groups):
+			a=4
+		if len(line.replace("|","").strip()) != len(gold_groups[r].replace("|","").strip()):
+			# Length mismatch, bug row
+			if ignore_diff_len:
+				if replace_diff_len:
+					line = gold_groups[r].replace("|","").strip()  # Replace prediction with gold length string with no segmentation
+				else:
+					continue
+		if line.strip() == gold_groups[r]:
+			perfect += 1
+		for i, c in enumerate(list(line.strip())):
+			counter += 1
+			if i == len(line.strip()) - 1:  # Last character is trivial, ignore
+				continue
+			if c == "|":
+				if len(line.strip()) > 1 and i == 0:
+					print("Complex token begins with boundary marker at pred row " + str(r))
+					sys.exit()
+				counter -=1
+				pred[-1] = 1
+			else:
+				pred.append(0)
+
+	pred_chars = counter
+
+	if pred_chars != gold_chars:
+		sys.stderr.write("ERROR: found " + str(gold_chars) + " gold chars but " + str(pred_chars) + " pred chars\n")
+		sys.exit()
+
+	true_positive = 0
+	false_positive = 0
+	false_negative = 0
+	for i in range(len(gold)):
+		if gold[i] == 0:
+			if pred[i] == 0:
+				pass
+			else:
+				false_positive += 1
+		else:
+			if pred[i] == 0:
+				false_negative += 1
+			else:
+				true_positive += 1
+
+	try:
+		precision = true_positive / (float(true_positive) + false_positive)
+	except Exception as e:
+		precision = 0
+
+	try:
+		recall = true_positive / (float(true_positive) + false_negative)
+	except Exception as e:
+		recall = 0
+
+	try:
+		f_score = 2 * (precision * recall) / (precision + recall)
+	except:
+		f_score = 0
+
+	try:
+		perf_score = perfect/float(total)
+	except:
+		perf_score = 0
+
+	if not silent:
+		print("Total chars: " + str(pred_chars))
+		print("Perfect groups: " + str(perf_score))
+		print("Precision: " + str(precision))
+		print("Recall: " + str(recall))
+		print("F-Score: " + str(f_score))
+
+	ret_dict = {"chars":pred_chars,"groups":total,"acc":perf_score,"precision": precision,"recall":recall,"f1":f_score}
+	return ret_dict
+
+
+if __name__ == "__main__":
+	p = argparse.ArgumentParser()
+
+	p.add_argument("goldfile")
+	p.add_argument("predfile")
+
+	opts = p.parse_args()
+
+	main(opts.goldfile,opts.predfile)
