0b37f4df55fb5109e2696479d724aa1276a87d1f
amir-zeldes <amir.zeldes@georgetown.edu>
2019-07-02 14:15:28 -0400

    Update paths, add marmot+stanford+ensemble

--- eval/eval_tagging.py
+++ eval/eval_tagging.py
@@ -6,7 +6,11 @@ from utils.eval_utils import list_files
 from collections import defaultdict
 from six import iterkeys
 from tt import train_tt, tag_tt
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib')))
+
 from marmot import train_marmot, tag_marmot
+from stanford import train_stanford, tag_stanford
 
 PY3 = sys.version_info[0] == 3
 
@@ -60,19 +64,39 @@ def run_eval(train_list, test_list, tagger="tt", retrain=True):
 	if not PY3:
 		train = unicode(train)
 
+	#with io.open("_tmp_train_tag.txt",'w',encoding="utf8") as f:
+	#	f.write(train)
+
 	if retrain:
-		if tagger == "marmot":
+		if tagger in ["marmot", "ensemble"]:
 			train_marmot(train)
-		else:
+		if tagger in ["stanford","ensemble"]:
+			train_stanford(train)
+		if tagger in ["tt","ensemble"]:
 			train_tt(train)
 
 	to_tag = [line.split("\t")[0] for line in test.strip().split("\n")]
 	golds = [line.split("\t") for line in test.strip().split("\n")]
 	if tagger == "marmot":
 		preds = tag_marmot("\n".join(to_tag)+"\n")
+	elif tagger== "stanford":
+		preds = tag_stanford("\n".join(to_tag)+"\n")
+	elif tagger == "ensemble":
+		marm = tag_marmot("\n".join(to_tag)+"\n")
+		marm = [line.split("\t") for line in marm.replace("\r","").strip().split("\n")]
+		stan = tag_stanford("\n".join(to_tag)+"\n")
+		stan = [line.split("\t") for line in stan.replace("\r","").strip().split("\n")]
+		tt = tag_tt("\n".join(to_tag)+"\n")
+		tt = [line.split("\t") for line in tt.replace("\r","").strip().split("\n")]
+		preds = marm
+		for i in range(len(tt)):
+			if tt[i][1] != preds[i][1] and tt[i][1] == stan[i][1]:
+				preds[i][1] = tt[i][1]
 	else:
 		preds = tag_tt("\n".join(to_tag)+"\n")
-	preds = [line.split("\t") for line in preds.replace("\r","").strip().split("\n")]
+
+	if tagger != "ensemble":
+		preds = [line.split("\t") for line in preds.replace("\r","").strip().split("\n")]
 
 	errs = defaultdict(int)
 	total = 0
@@ -84,6 +108,7 @@ def run_eval(train_list, test_list, tagger="tt", retrain=True):
 		gold = golds[i][1]
 		if pos != gold:
 			errs[tok + "\t" + gold + "\t"+ pos] += 1
+			#errs[tok + "\t" + gold + "\t"+ pos + "\t" + preds[i-1][0] ] += 1  # Replace this with above for err context
 		else:
 			correct +=1
 		if gold =="PREP": # Most frequent tag
@@ -105,7 +130,8 @@ if __name__ == "__main__":
 	p.add_argument("--train_list",default=None,help="file with one file name per line of TT SGML training files; all files not in test if not supplied")
 	p.add_argument("--test_list",default="test_list.tab",help="file with one file name per line of TT SGML test files")
 	p.add_argument("--file_dir",default="tt",help="directory with TT SGML files")
-	p.add_argument("--tagger",default="tt",help="which tagger to us",choices=["tt","marmot"])
+	p.add_argument("--method",default="tt",help="which tagger to use",choices=["tt","marmot","stanford","ensemble"])
+	p.add_argument("--retrain",action="store_true",help="whether to retrain")
 
 	opts = p.parse_args()
 
@@ -126,4 +152,4 @@ if __name__ == "__main__":
 		train_list = [os.path.basename(f) for f in train_list if os.path.basename(f) not in test_list]
 		train_list = [script_dir + opts.file_dir + os.sep + f for f in train_list]
 
-	run_eval(train_list,test_list,tagger=opts.tagger)
+	run_eval(train_list,test_list,tagger=opts.method,retrain=opts.retrain)
