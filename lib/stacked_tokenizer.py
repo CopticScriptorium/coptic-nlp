b572bb5625ad32cbbf1e0b663592126962dd6a20
amir-zeldes <amir.zeldes@georgetown.edu>
2019-07-17 13:47:37 -0400

    Add metatokenizer and normalization call

--- lib/stacked_tokenizer.py
+++ lib/stacked_tokenizer.py
@@ -8,6 +8,7 @@ from six import iterkeys, itervalues
 PY3 = sys.version_info[0] == 3
 
 stk_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = stk_dir + os.sep + ".." + os.sep + "data" + os.sep
 
 cap_map = {"Ⲁ":"ⲁ","Ⲃ":"ⲃ","Ⲑ":"ⲑ","Ⲇ":"ⲇ","Ⲉ":"ⲉ","Ϥ":"ϥ","Ⲅ":"ⲅ","Ⲏ":"ⲏ","Ⲓ":"ⲓ","Ϫ":"ϫ","Ⲕ":"ⲕ","Ⲗ":"ⲗ","Ⲙ":"ⲙ","Ⲛ":"ⲛ","Ⲟ":"ⲟ","Ⲡ":"ⲡ","Ϭ":"ϭ","Ⲣ":"ⲣ","Ⲥ":"ⲥ","Ⲧ":"ⲧ","Ⲩ":"ⲩ","Ⲫ":"ⲫ","Ⲱ":"ⲱ","Ϩ":"ϩ","Ⲭ":"ⲭ","Ⲍ":"ⲍ","Ϣ":"ϣ","Ϯ":"ϯ","Ⲯ":"ⲯ","Ⲝ":"ⲝ"}
 
@@ -16,11 +17,13 @@ try:
 	from .tokenize_lookup import lookup_tokenize
 	from .tokenize_rf import RFTokenizer
 	from .tokenize_morph import MorphAnalyzer
+	from .tokenize_meta import MetaTokenizer
 except:
 	from tokenize_fs import fs_tokenize
 	from tokenize_lookup import lookup_tokenize
 	from tokenize_rf import RFTokenizer
 	from tokenize_morph import MorphAnalyzer
+	from tokenize_meta import MetaTokenizer
 
 from argparse import ArgumentParser
 from six import iteritems
@@ -32,25 +35,28 @@ if not PY3:
 
 sys.path.append("lib")
 
+from auto_norm import normalize as lookup_normalize
+
 class BoundGroup:
 
-	# Static list of characters that are removed from norm text (basic normalization)
-	orig_chars = ["̈", "", "̄", "̀", "̣", "`", "̅", "̈", "̂", "︤", "︥", "︦", "⳿", "~", "\n", "[", "]", "̇", "᷍"]
+	# Static list of characters that are removed from clean text (basic normalization)
+	orig_chars = ["̈", "", "̄", "̀", "̣", "`", "̅", "̈", "̂", "︤", "︥", "︦", "⳿", "~", "\n", "[", "]", "̇", "᷍", "⸍", "›", "‹"]
 	tokenization_chars = ["|", "-"]
 
 	def __init__(self, out_tok_sep="|"):
-		self.dirty = ""
-		self.norm = ""
-		self.orig = ""
-		self.tokenization = ""  # Holds the clean tokenization (a|f|sOtm)
+		self.dirty = ""  # Holds input string of bound group with diacritics, XML and whitespace
+		self.clean = ""  # Holds cleaned input string, without diacritics or XML/whitespace
+		self.norm = ""   # Holds normalized version of bound group without XML/whitespace
+		self.orig = ""   # Holds unnormalized, diplomatic version of bound group, with diacritics but no XML/whitespace
+		self.tokenization = ""  # Holds the normalized tokenization (a|f|sOtm)
 		self.tokenized = ""  # Holds the dirty tokenization (a^|f|sO<hi rend="x">t</hi>m)
 		self.unaligned_tokenization = ""  # Holds irregular, unalignable tokenization only expressible in SGML (n|t|he)
 		self.pretokenization = ""  # Holds pre-specified tokenization (pipes in input)
 		self.cursor = 0
-		self.norm_cursor = 0
+		self.clean_cursor = 0
 		self.mode = "text"
-		self.norm_map = {}  # Dictionary from clean character position to dirty position, without seps
-		self.merge_boundaries = []  # List of norm_map positions at which bound group merging has happened (during 'laytonization')
+		self.clean_map = {}  # Dictionary from clean character position to dirty position, without seps
+		self.merge_boundaries = []  # List of clean_map positions at which bound group merging has happened (during 'laytonization')
 		self.out_tok_sep = out_tok_sep  # Separator to print between subtokens in 'pipes' mode
 		self.proclitic = False  # Whether to join with next group ('laytonize')
 
@@ -78,18 +84,18 @@ class BoundGroup:
 		if self.mode != "xml":
 			if c in self.orig_chars:
 				self.orig = "".join([self.orig,c])
-				#self.norm_map[self.norm_cursor] = self.cursor
+				#self.clean_map[self.clean_cursor] = self.cursor
 			else:
 				self.orig = "".join([self.orig,c])
 				if c == "⳯":  # ad-hoc normalization of superscript ni chracter
 					c = "ⲛ"
 				elif c in cap_map:
 					c = cap_map[c]
-				self.norm  = "".join([self.norm,c])
+				self.clean  = "".join([self.clean, c])
 				if c not in ["□","■"] and tokenized:
 					self.pretokenization  = "".join([self.pretokenization,c])
-				self.norm_map[self.norm_cursor] = self.cursor
-				self.norm_cursor += 1
+				self.clean_map[self.clean_cursor] = self.cursor
+				self.clean_cursor += 1
 
 		if c == ">":
 			self.mode = "text"
@@ -101,36 +107,30 @@ class BoundGroup:
 		self.tokenization = tokenization
 		# Construct approximate tokenization for piped output
 		len_offset = 0
-		if "ⲑ" in self.dirty and ("ⲧ|ϩ" in tokenization or "ⲧ-ϩ" in tokenization):
+		if ("ⲑ" in self.dirty or "Ⲑ" in self.dirty) and ("ⲧ|ϩ" in tokenization or "ⲧ-ϩ" in tokenization):
 			tokenization = tokenization.replace("ⲧ|ϩ","ⲑ|").replace("ⲧ-ϩ","ⲑ-")
+		if ("ⲫ" in self.dirty or "Ⲫ" in self.dirty) and ("ⲡ|ϩ" in tokenization or "ⲡ-ϩ" in tokenization):
+			tokenization = tokenization.replace("ⲡ|ϩ","ⲫ|").replace("ⲡ-ϩ","ⲫ-")
 		if "ϯ" in self.dirty and "ⲧ|ⲓ" in tokenization:
 			tokenization = tokenization.replace("ⲧ|ⲓ","|ϯ")
 		if "ⲉⲧⲛⲁ" in self.dirty and "ⲉⲧⲛ|ⲛⲁ" in tokenization and "ⲉⲧⲛⲛⲁ" not in self.dirty:
 			tokenization = tokenization.replace("ⲉⲧⲛ|ⲛⲁ","ⲉⲧⲛ|ⲁ")
 		tokenized = self.dirty
 		tok_rep = tokenization.replace("|","").replace("-","").strip()
-		if len(self.norm.strip()) != len(tok_rep):
-			if len(self.norm.strip()) + len_offset > len(tok_rep):
+		if len(self.clean.strip()) != len(tok_rep):  # Tokenization resulted in different number of characters than orig
+			tokenization = self.reconcile_tokenization(self.clean.strip(), tokenization)
+			if tokenization != "":
 				self.unaligned_tokenization = tokenization
-				tokenization = self.reconcile_tokenization(self.norm.strip(),tokenization)
-				# Tokenization resulted in dropping characters
-				#with io.open("errs.txt",'a',encoding="utf8") as f:
-				#	f.write("WARN: unit '" + self.norm + "' tokenization shorter than input '" + tok_rep + "' (ignored)\n")
-				#tokenization = self.norm
-				self.tokenization = tokenization
-			else:
-				# Tokenization resulted in adding characters
-				self.unaligned_tokenization = tokenization
-				tokenization = self.reconcile_tokenization(self.norm.strip(),tokenization)
-				#with io.open("errs.txt",'a',encoding="utf8") as f:
-				#	f.write("WARN: unit '" + self.norm + "' tokenization longer than input '" + tok_rep + "' (ignored)\n")
-				#tokenization = self.norm
-				self.tokenization = tokenization
-		cursor = len(self.norm) - 1
+			else:  # Could not align normalized tokenization, revert everything to unsegmented clean form
+				sys.stderr.buffer.write(("WARN: Could not reconcile normalized tokenization: " + self.clean.strip() + "<>" + self.tokenization + "\n").encode("utf8"))
+				self.tokenization = self.unaligned_tokenization = self.norm = self.clean
+		cursor = len(self.clean) - 1
+		if self.unaligned_tokenization != "":
+			tokenization = self.unaligned_tokenization
 		for i in range(1, len(tokenization)):
 			c = tokenization[-i]  # Traverse tokenized string backwards
 			if c in self.tokenization_chars:
-				position = self.norm_map[cursor+1] - 1 if cursor < len(self.norm) else self.norm_map[cursor]
+				position = self.clean_map[cursor + 1] - 1 if cursor < len(self.clean) else self.clean_map[cursor]
 				tokenized = "".join([tokenized[:position+1], c, tokenized[position+1:]])
 			else:
 				cursor -= 1
@@ -143,9 +143,9 @@ class BoundGroup:
 		offset = 0
 		if unsegmented == "":
 			return ""
-		for i, c in enumerate(list(unsegmented)):
+		for i, c in enumerate(list(unsegmented)):  # Try scanning through norm and finding all letters of orig (orig is shorter)
 			if len(segmented) <= i+1+offset:
-				if i+1 < len(unsegmented):  # Segmentation is over, append remaining characters to output
+				if i+1 <= len(unsegmented):  # Segmentation is over, append remaining characters to output
 					output = "".join([output,unsegmented[i:]])
 				break
 			else:
@@ -164,16 +164,42 @@ class BoundGroup:
 					#if segmented[i+offset] == unsegmented[i]:
 					#	output+=unsegmented[i]
 				output += c
+		if "||" in output:  # Reconciliation has failed, try scanning through orig and finding all letters of norm (normalization is shorter)
+			output = ""
+			offset = 0
+			unseg_offset = 0
+			for i, c in enumerate(list(unsegmented)):
+				if i+unseg_offset == len(unsegmented):
+					break
+				if segmented[i+offset] in ["|","-"]:
+					output = "".join([output,segmented[i+offset]])
+					offset += 1
+				if segmented[i+offset] != unsegmented[i+unseg_offset]:  # Character conflict
+					while segmented[i+offset] != unsegmented[i+unseg_offset]:
+						if i+unseg_offset == len(unsegmented)-1:
+							break
+						elif segmented[i+offset] in ["|","-"]:
+							output = "".join([output, segmented[i + offset]])
+							offset +=1
+						else:
+							output = "".join([output,unsegmented[i+unseg_offset]])
+							unseg_offset += 1
+					output = "".join([output,unsegmented[i+unseg_offset]])
+				else:
+					output += unsegmented[i+unseg_offset]
+
 		output = re.sub(r'\|+','|',output)
 		output = re.sub(r'-+','-',output)
 		if output[-1] in ["|","-"]:
 			output = output[:-1]
 		if output[0] in ["|","-"]:
 			output = output[1:]
+		if output.count("|") != segmented.count("|"):
+			output=""
 		return output
 
 	def serialize_sgml(self):
-		out_sgml = '<norm_group norm_group="' + self.norm.replace("□","").replace("■","") + '">'
+		out_sgml = '<norm_group norm_group="' + self.norm.replace("□", "").replace("■", "") + '">'
 		if "||" in self.tokenized:
 			self.tokenized = re.sub(r'\|\|(.)',r'|\1|',self.tokenized)  # Try to push boundary to next letter
 		if "||" in self.tokenized and "||" not in self.tokenization:
@@ -236,7 +262,10 @@ class BoundGroup:
 				raise AttributeError("No attribute: " + str(item))
 
 	def __repr__(self):
-		return self.norm + " (" + self.dirty.replace("\\n","\n") + ")"
+		if len(self.norm)==0:
+			return "C: " + self.clean + " (" + self.dirty.replace("\\n", "\n") + ")"
+		else:
+			return "N: " + self.norm + " (" + self.dirty.replace("\\n", "\n") + ")"
 
 
 def add_lines(text):
@@ -256,16 +285,13 @@ def dissolve(text, tok_sep="|", tokenized=False):
 	bound_groups = [BoundGroup(out_tok_sep=tok_sep)]  # First bound group filled as position of pre-text XML
 
 	for i, c in enumerate(text):
-		if c == "]":
-			a=4
 		if mode != "xml" and (c in [" ", "_"] and i != len(text)-1):  # bound group ends
-			if bound_groups[-1].norm != "" or bound_groups[-1].dirty.strip() in ["[","]"]: # Check previous group is non-empty
-				if bound_groups[-1].norm == "":  # Exception for user entered square bracket group
-					bound_groups[-1].norm = bound_groups[-1].dirty.strip()
+			if bound_groups[-1].clean != "" or bound_groups[-1].dirty.strip() in ["[", "]"]: # Check previous group is non-empty
+				if bound_groups[-1].clean == "":  # Exception for user entered square bracket group
+					bound_groups[-1].clean = bound_groups[-1].dirty.strip()
 				bound_groups.append(BoundGroup(out_tok_sep=tok_sep))
 		else:
 			bound_groups[-1].affix(c,tokenized)
-
 
 		if c == "<":
 			mode = "xml"
@@ -273,7 +299,7 @@ def dissolve(text, tok_sep="|", tokenized=False):
 			if c == ">":
 				mode = "text"
 
-	if bound_groups[-1].norm.strip() == "":  # Trailing white space removal
+	if bound_groups[-1].clean.strip() == "":  # Trailing white space removal
 		if bound_groups[-1].dirty.strip() != "":
 			# Preserve non-token material in penultimate bound group
 			for c in bound_groups[-1].dirty:
@@ -286,11 +312,11 @@ def dissolve(text, tok_sep="|", tokenized=False):
 def find_non_sep_position(text, norm_position, norm_sep="|", morph_sep="-"):
 	"""
 	Finds the actual position of character n in a string, such that n is the
-	number of characters traversed excluding separator chatacters
+	number of characters traversed excluding separator characters
 
 	:param text: the text to search for a position in
 	:param norm_position: position ignoring separators
-	:param norm_sep: norm separator to ignore when counting, default '|'
+	:param norm_sep: clean separator to ignore when counting, default '|'
 	:param norm_sep: morph separator to ignore when counting, default '-'
 	:return: position in string not ignoring separators
 	"""
@@ -336,7 +362,7 @@ def adjust_theta(tokenization):
 
 class StackedTokenizer:
 
-	def __init__(self,lines=False,pipes=False,tokenized=False,no_morphs=False,detok=0,segment_merged=False,model="cop",ambig=None):
+	def __init__(self,lines=False,pipes=False,tokenized=False,no_morphs=False,detok=0,segment_merged=False,model="cop",ambig=None,use_meta=False):
 		self.lines = lines
 		self.pipes = pipes
 		self.tokenized = tokenized
@@ -347,6 +373,9 @@ class StackedTokenizer:
 		if ambig is not None:
 			self.load_ambig(ambig)
 		self.load_rftokenizer()
+		self.use_meta = use_meta
+		if self.use_meta:
+			self.metatok = MetaTokenizer(model="cop",rf_tok=self.rf_tok)
 		# Place-holders for detokenization mode (a.k.a. Laytonization)
 		self.detokenize = detok
 		self.segment_merged = segment_merged
@@ -382,6 +411,18 @@ class StackedTokenizer:
 		self.rf_tok = RFTokenizer(model=self.model)
 		self.rf_tok.load()
 
+	def normalize(self,groups,norm_table=None):
+		cleans = [g.clean for g in groups]
+		if norm_table is None:
+			norms = lookup_normalize("\n".join(cleans),table_file=data_dir+"norm_table.tab").split("\n")
+		else:
+			norms = lookup_normalize("\n".join(cleans),table_file=norm_table).split("\n")
+
+		for i,g in enumerate(groups):
+			g.norm = norms[i]
+
+		return groups
+
 	@staticmethod
 	def merge_groups(last_grp, grp):
 		"""
@@ -391,20 +432,21 @@ class StackedTokenizer:
 		:param grp: following group to merge into
 		:return:
 		"""
-		max_map = max(iterkeys(last_grp.norm_map))
-		max_mapped = len(last_grp.dirty)-1 #max(itervalues(last_grp.norm_map))
+		max_map = max(iterkeys(last_grp.clean_map))
+		max_mapped = len(last_grp.dirty)-1 #max(itervalues(last_grp.clean_map))
 		new_group = last_grp
+		new_group.clean += grp.clean
 		new_group.norm += grp.norm
 		new_group.orig += grp.orig
 		new_group.dirty += grp.dirty
 		new_group.merge_boundaries.append(max_map)
-		for key in grp.norm_map:
-			new_group.norm_map[key + max_map + 1] = grp.norm_map[key] + max_mapped + 1
+		for key in grp.clean_map:
+			new_group.clean_map[key + max_map + 1] = grp.clean_map[key] + max_mapped + 1
 		new_group.proclitic = grp.proclitic
 
 		return new_group
 
-	def analyze(self,data):
+	def analyze(self,data,do_normalize=True,norm_table=None):
 		if self.lines:
 			data = add_lines(data)
 
@@ -414,14 +456,14 @@ class StackedTokenizer:
 
 		if self.detokenize:
 			for grp in grps:
-				if grp.norm in self.detok_table:
+				if grp.clean in self.detok_table:
 					grp.proclitic = True
 			last_grp = None
 			detokenized = []
 			for grp in grps:
 				if last_grp is not None:
-					if last_grp.proclitic or last_grp.norm in self.detok_table:
-						if grp.norm not in self.no_merge:
+					if last_grp.proclitic or last_grp.clean in self.detok_table:
+						if grp.clean not in self.no_merge:
 							new_grp = self.merge_groups(last_grp,grp)
 							last_grp = new_grp
 							continue
@@ -436,9 +478,15 @@ class StackedTokenizer:
 				plain_tokenization = g.pretokenization
 				plain_tokenization = adjust_theta(plain_tokenization)
 				g.orig = g.orig.replace("□", "").replace("■", "")
-				g.norm = g.norm.replace("□", "").replace("■", "")
+				g.clean = g.clean.replace("□", "").replace("■", "")
 				g.add_tokenization(plain_tokenization)
 		else:
+			if do_normalize:
+				grps = self.normalize(grps,norm_table=norm_table)
+			else:
+				for g in grps:
+					g.norm = g.clean
+
 			norms = [g.norm for g in grps]
 			norm_types = list(set(norms))
 
@@ -472,17 +520,22 @@ class StackedTokenizer:
 
 			unknown_indices = []
 			for i, norm in enumerate(norms):
-				if norm in unknown:
+				if norm in unknown:# or clean in self.ambig:
 					unknown_indices.append(i)
 
 			if not PY3:
 				pass
-			# norms = [norm.decode("utf8") for norm in norms]
+			# norms = [clean.decode("utf8") for clean in norms]
 			tokenizations_rf = self.rf_tok.rf_tokenize(norms, indices=unknown_indices)
+
+			if self.use_meta:
+				tokenizations_meta, believe_fs, meta_probas = self.metatok.predict(norms)
 
 			best_tokenizations = []
 			j = 0
 			for i, norm in enumerate(norms):
+				if norm== "ⲛⲁⲩ":
+					s=3
 				if i in unknown_indices:
 					if norm in self.ambig:
 						if tokenizations_rf[j] in self.ambig[norm]:
@@ -495,7 +548,12 @@ class StackedTokenizer:
 						best_tokenizations.append(tokenizations_rf[j])
 						j += 1
 				else:
-					best_tokenizations.append(known_tokenizations[norm])
+					if known_tokenizations[norm] in tokenizations_lo or not self.use_meta:
+						best_tokenizations.append(known_tokenizations[norm])
+					elif meta_probas[i] > 0.1:
+						best_tokenizations.append(known_tokenizations[norm])
+					else:
+						best_tokenizations.append(tokenizations_meta[i])
 
 			# blender_features = get_blender_features()
 
@@ -532,7 +590,7 @@ if __name__ == "__main__":
 	options = parser.parse_args()
 
 	stk = StackedTokenizer(lines=options.lines,pipes=options.pipes,tokenized=options.tokenized,no_morphs=options.no_morphs,
-						   detok=options.detokenize, segment_merged=options.segment_merged)
+						   detok=options.detokenize, segment_merged=options.segment_merged,ambig=data_dir+"ambig.tab")
 
 	data = io.open(options.infile, encoding="utf8").read().replace("\r", "")
 
