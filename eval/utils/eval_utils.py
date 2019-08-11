498df85905ec2785361bee6d91e9f0a7b314944b
amir-zeldes <amir.zeldes@georgetown.edu>
2019-08-11 04:54:08 -0400

    Rename onno documents

--- eval/utils/eval_utils.py
+++ eval/utils/eval_utils.py
@@ -115,10 +115,13 @@ def list_files(alias="silver",file_dir=None,mode=None):
 			file_list = [file_dir + "BritMusOriental6783_23a_27a.txt"]
 		elif alias.lower().startswith("onno"):  # onnophrius
 			file_dir = eval_dir + "plain" + os.sep
-			file_list = [file_dir + "apa_onnophrius_part1.txt"]
+			file_list = [file_dir + "BritMusOriental7027_01a_07b.txt"]
 		elif alias.lower().startswith("ephraim"):
 			file_dir = eval_dir + "plain" + os.sep
 			file_list = [file_dir + "BritMusOriental6783_63b_67b.txt"]
+		elif alias.lower().startswith("victor2"):
+			file_dir = eval_dir + "plain" + os.sep
+			file_list = [file_dir + "martyrdom.victor.02.txt"]
 		elif alias.lower().startswith("victor"):
 			file_dir = eval_dir + "plain" + os.sep
 			file_list = [file_dir + "martyrdom.victor.txt"]
@@ -132,28 +135,30 @@ def list_files(alias="silver",file_dir=None,mode=None):
 			file_dir = eval_dir + "plain" + os.sep
 			file_list = [file_dir + "BritMusOriental6783_23a_27a.txt",
 						 file_dir + "BritMusOriental6783_63b_67b.txt",
-						 file_dir + "apa_onnophrius_part1.txt"]
+						 file_dir + "BritMusOriental7027_01a_07b.txt"]
 		elif alias.lower().startswith("vicephon"):
 			file_dir = eval_dir + "plain" + os.sep
 			file_list = [file_dir + "martyrdom.victor.txt",
 						 file_dir + "BritMusOriental6783_63b_67b.txt",
-						 file_dir + "apa_onnophrius_part1.txt"]
+						 file_dir + "BritMusOriental7027_01a_07b.txt"]
 		elif alias.lower().startswith("viccyon"):
 			file_dir = eval_dir + "plain" + os.sep
 			file_list = [file_dir + "martyrdom.victor.txt",
 						 file_dir + "BritMusOriental6783_23a_27a.txt",
-						 file_dir + "apa_onnophrius_part1.txt"]
+						 file_dir + "BritMusOriental7027_01a_07b.txt"]
 	else:
 		if alias.lower() == "cyrus":
 			file_dir = eval_dir + "unreleased" + os.sep
 			file_list = [file_dir + "BritMusOriental6783_23a_27a.tt"]
 		elif alias.lower().startswith("onno"):  # onnophrius
 			file_dir = eval_dir + "unreleased" + os.sep
-			file_list = [file_dir + "apa_onnophrius_part1.tt"]
-
+			file_list = [file_dir + "BritMusOriental7027_01a_07b.tt"]
 		elif alias.lower().startswith("ephraim"):
 			file_dir = eval_dir + "unreleased" + os.sep
 			file_list = [file_dir + "BritMusOriental6783_63b_67b.tt"]
+		elif alias.lower().startswith("victor2"):
+			file_dir = eval_dir + "unreleased" + os.sep
+			file_list = [file_dir + "martyrdom.victor.02.tt"]
 
 		#!! DANGER: this is in plain, not unreleased
 		elif alias.lower().startswith("victor"):
@@ -180,20 +185,20 @@ def list_files(alias="silver",file_dir=None,mode=None):
 			file_dir = eval_dir + "unreleased" + os.sep
 			file_list = [file_dir + "BritMusOriental6783_23a_27a.tt",
 						 file_dir + "BritMusOriental6783_63b_67b.tt",
-						 file_dir + "apa_onnophrius_part1.tt"]
+						 file_dir + "BritMusOriental7027_01a_07b.tt"]
 		elif alias.lower() == "vicephon_tt":
 			file_dir = eval_dir + "plain" + os.sep
 			file_list = [file_dir + "martyrdom.victor.01.tt"]
 			file_dir = eval_dir + "unreleased" + os.sep
 			file_list += [file_dir + "BritMusOriental6783_63b_67b.tt",
-						  file_dir + "apa_onnophrius_part1.tt"]
+						  file_dir + "BritMusOriental7027_01a_07b.tt"]
 
 		elif alias.lower() == "viccyon_tt":
 			file_dir = eval_dir + "plain" + os.sep
 			file_list = [file_dir + "martyrdom.victor.01.tt"]
 			file_dir = eval_dir + "unreleased" + os.sep
 			file_list += [file_dir + "BritMusOriental6783_23a_27a.tt",
-						  file_dir + "apa_onnophrius_part1.tt"]
+						  file_dir + "BritMusOriental7027_01a_07b.tt"]
 		elif alias.lower() == "silver":
 			test_list = io.open(eval_dir + "test_list.tab").read().strip().split("\n")
 			file_list = glob(file_dir + os.sep + "*.tt")
