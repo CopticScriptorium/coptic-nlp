0b527a6be7574197bb9486ea1a876fc16a17df76
amir-zeldes <amir.zeldes@georgetown.edu>
2019-08-29 11:22:18 -0400

    Add updipe, pre- and postprocessor   * Supported methods are now malt and udpipe (requires udpipe executable in bin/)   * Added depedit preprocessing and postprocessing transformations     * Preprocessing adds rule based morphology to parser input, currently not helpful     * Postprocessing corrects some improbable parser outputs (does help)

--- eval/eval_parsing.py
+++ eval/eval_parsing.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from argparse import ArgumentParser
 import sys, io, os, re, tempfile, subprocess
 from depedit import DepEdit
@@ -5,18 +8,35 @@ from depedit import DepEdit
 script_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep
 bin_dir = script_dir + ".." + os.sep + "bin" + os.sep
 data_dir = script_dir + ".." + os.sep + "data" + os.sep
-parser_path = bin_dir + "maltparser-1.8" + os.sep
 
 PY3 = sys.version_info[0] == 3
 
 
-def exec_via_temp(input_text, command_params, workdir=""):
+def nomisc(conllu):
+	output = []
+	lines = conllu.split("\n")
+	for line in lines:
+		if "\t" in line:
+			fields = line.split("\t")
+			fields[-1] = "_"
+			line = "\t".join(fields)
+		output.append(line)
+	return "\n".join(output)
+
+
+def exec_via_temp(input_text, command_params, workdir="", input2=None):
 	temp = tempfile.NamedTemporaryFile(delete=False)
 	exec_out = ""
 	try:
 		temp.write(input_text.encode("utf8"))
 		temp.close()
+		temp2 = None
 
+		if any(["tempfilename2" in param for param in command_params]):
+			temp2 = tempfile.NamedTemporaryFile(delete=False)
+			temp2.write(input2.encode("utf8"))
+			temp2.close()
+			command_params = [x if 'tempfilename2' not in x else x.replace("tempfilename2",temp2.name) for x in command_params]
 		command_params = [x if x != 'tempfilename' else temp.name for x in command_params]
 		if workdir == "":
 			proc = subprocess.Popen(command_params, stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -30,14 +50,19 @@ def exec_via_temp(input_text, command_params, workdir=""):
 		print(e)
 	finally:
 		os.remove(temp.name)
+		if temp2 is not None:
+			os.remove(temp2.name)
 		if PY3:
 			exec_out = exec_out.decode("utf8")
 		return exec_out
 
 
-def run_eval(train_list, test_list, retrain=True, method="malt"):
+def run_eval(train_list, test_list, dev_list=None, retrain=True, method="malt", preprocess=False, postprocess=False):
 
-	d = DepEdit(config_file=data_dir + "add_ud_and_flat_morph.ini",options=type('', (), {"quiet":True,"kill":"both"})())
+	if preprocess:
+		d = DepEdit(config_file=data_dir + "add_ud_and_flat_morph.ini",options=type('', (), {"quiet":True,"kill":"both"})())
+	else:
+		d = DepEdit(options=type('', (), {"quiet":True,"kill":"both"})())
 
 	train = ""
 	for file_ in train_list:
@@ -45,6 +70,7 @@ def run_eval(train_list, test_list, retrain=True, method="malt"):
 
 	train = train.split("\n")
 	train = d.run_depedit(train)
+	train = nomisc(train)
 
 	test_gold = ""
 	for file_ in test_list:
@@ -55,6 +81,11 @@ def run_eval(train_list, test_list, retrain=True, method="malt"):
 	test_gold = d.run_depedit(test_gold.split("\n"))
 	test_gold = test_gold.split("\n")
 
+	if dev_list is not None:
+		dev_gold = ""
+		for file_ in dev_list:
+			dev_gold += io.open(file_,encoding="utf8").read()
+		dev_gold = d.run_depedit(dev_gold.split("\n"))
 
 	test_blank = []
 	for line in test_gold:
@@ -65,18 +96,40 @@ def run_eval(train_list, test_list, retrain=True, method="malt"):
 
 	test_blank = d.run_depedit(test_blank)
 
-	cmd = ["java","-jar","maltparser-1.8.jar","-c","temp","-i","tempfilename",
-		   "-F",r"C:\Uni\Coptic\git\corpora\treebank-dev\v2.1parser\addMergPOSTAGS0FORMStack0.xml",
-		   "-m","learn","-grl","root","-pcr","none","-a","nivrestandard","-pp","head","-nr",
-		   "true","-ne","false"]
+	if method == "malt":
+		parser_path = bin_dir + "maltparser-1.8" + os.sep
 
-	# Train the parser
-	exec_via_temp(train,cmd,parser_path)
+		if retrain:
+			cmd = ["java","-jar","maltparser-1.8.jar","-c","temp","-i","tempfilename",
+				   "-F",data_dir+"addMergPOSTAGS0FORMStack0.xml",
+				   "-m","learn","-grl","root","-pcr","none","-a","nivrestandard",  # "stacklazy"
+				   "-pp","head","-nr",
+				   "true","-ne","false"]
 
-	# Test
-	cmd = ['java','-mx512m','-jar',"maltparser-1.8.jar",'-c','temp','-i','tempfilename','-m','parse']
-	parsed = exec_via_temp(test_blank,cmd,parser_path)
-	parsed = parsed.replace("\r","")
+			# Train the parser
+			exec_via_temp(train,cmd,parser_path)
+
+		# Test
+		cmd = ['java','-mx512m','-jar',"maltparser-1.8.jar",'-c','temp','-i','tempfilename','-m','parse']
+		parsed = exec_via_temp(test_blank,cmd,parser_path)
+		parsed = parsed.replace("\r","")
+	elif method == "udpipe":
+		parser_path = bin_dir  # Should contain udpipe executable and cop.udpipe
+
+		if retrain:
+			# Train the parser
+			cmd = [parser_path+"udpipe", "--train", "cop.udpipe","--heldout=tempfilename2","--tagger=none","--tokenizer==none","--transition_system=swap","--embedding_form_file="+parser_path+"cop.vec","tempfilename"]
+			exec_via_temp(train,cmd,parser_path,input2=dev_gold)
+
+		# Test
+		cmd = [parser_path+'udpipe','--input','conllu','--parse','cop.udpipe','tempfilename']
+		parsed = exec_via_temp(test_blank,cmd,parser_path)
+		parsed = parsed.replace("\r","")
+
+	# Postprocessing
+	if postprocess:
+		d = DepEdit(config_file=data_dir + "postprocess_parser.ini",options=type('', (), {"quiet":True,"kill":"both"})())
+		parsed = d.run_depedit(parsed.split("\n"))
 
 	total=0
 	correct_head=0
@@ -105,6 +158,11 @@ def run_eval(train_list, test_list, retrain=True, method="malt"):
 
 	results = {"acc":acc, "lab":lab, "attach":attach}
 
+	with io.open(script_dir + "errors" + os.sep + "parse_pred.conll10",'w',encoding="utf8",newline="\n") as f:
+		f.write(parsed)
+	with io.open(script_dir + "errors" + os.sep + "parse_gold.conll10",'w',encoding="utf8",newline="\n") as f:
+		f.write(nomisc("\n".join(test_gold)))
+
 	return results
 
 
@@ -112,9 +170,12 @@ if __name__ == "__main__":
 	p = ArgumentParser()
 	p.add_argument("--train_list",default="ud_dev_train",help="file with one file name per line of conll10 training files or alias such as 'ud_train', 'ud_dev_train'")
 	p.add_argument("--test_list",default="ud_test",help="file with one file name per line of conll10 training files or alias such as 'ud_train', 'ud_dev_train'")
+	p.add_argument("--dev_list",default="ud_test_mini",help="file with one file name per line of conll10 training files or alias such as 'ud_dev', 'ud_mini_test")
 	p.add_argument("--file_dir",default=None,help="directory with Coptic Treebank files")
-	p.add_argument("--method",default="malt",choices=["malt"],help="parser to use")
+	p.add_argument("--method",default="malt",choices=["malt","udpipe"],help="parser to use")
 	p.add_argument("--retrain",action="store_true",help="whether to retrain the parser")
+	p.add_argument("--preprocess",action="store_true",help="whether to preprocess parser output with depedit")
+	p.add_argument("--postprocess",action="store_true",help="whether to postprocess parser output with depedit")
 
 	opts = p.parse_args()
 
@@ -130,6 +191,10 @@ if __name__ == "__main__":
 		test_list = [script_dir + opts.file_dir + os.sep + f for f in test_list]
 	elif opts.test_list.lower() == "ud_test":
 		test_list = [file_dir + "cop_scriptorium-ud-test.conllu"]
+	elif opts.test_list.lower() == "ud_dev":
+		test_list = [file_dir + "cop_scriptorium-ud-dev.conllu"]
+	elif opts.test_list.lower() == "ud_train":
+		test_list = [file_dir + "cop_scriptorium-ud-train.conllu"]
 	else:
 		sys.stderr.write("o Unknown test set: " + str(opts.test_list) + "\n")
 		sys.exit(0)
@@ -141,8 +206,23 @@ if __name__ == "__main__":
 		train_list = [file_dir + "cop_scriptorium-ud-train.conllu"]
 	elif opts.train_list.lower() == "ud_dev_train":
 		train_list = [file_dir + "cop_scriptorium-ud-train.conllu",file_dir + "cop_scriptorium-ud-dev.conllu"]
+	elif opts.train_list.lower() == "ud_dev_test" or opts.train_list.lower() == "ud_dev+ud_test":
+		train_list = [file_dir + "cop_scriptorium-ud-test.conllu",file_dir + "cop_scriptorium-ud-dev.conllu"]
+	elif opts.train_list.lower() == "ud_train_test" or opts.train_list.lower() == "ud_train+ud_test":
+		train_list = [file_dir + "cop_scriptorium-ud-test.conllu",file_dir + "cop_scriptorium-ud-train.conllu"]
+	elif opts.train_list.lower() == "ud_all":
+		train_list = [file_dir + "cop_scriptorium-ud-test.conllu",file_dir + "cop_scriptorium-ud-train.conllu",file_dir + "cop_scriptorium-ud-dev.conllu"]
 	else:
 		sys.stderr.write("o Unknown train set: " + str(opts.train_list) + "\n")
 		sys.exit(0)
 
-	run_eval(train_list,test_list,retrain=opts.retrain)
+	if os.path.isfile(opts.dev_list):
+		dev_list = io.open(opts.dev_list,encoding="utf8").read().strip().split("\n")
+		dev_list = [script_dir + opts.file_dir + os.sep + f for f in dev_list]
+	elif opts.dev_list.lower() == "ud_test_mini":
+		dev_list = [file_dir + "cop_scriptorium-ud-test_mini.conllu"]
+	else:
+		sys.stderr.write("o Unknown dev set: " + str(opts.dev_list) + "\n")
+		sys.exit(0)
+
+	run_eval(train_list, test_list, dev_list=dev_list, method=opts.method, retrain=opts.retrain, preprocess=opts.preprocess, postprocess=opts.postprocess)
