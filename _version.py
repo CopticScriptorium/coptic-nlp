2bb61b89fa093c935f6927a949017cee0adbad07
amir-zeldes <amir.zeldes@georgetown.edu>
2019-09-20 12:09:49 -0400

    V3.0.0

--- _version.py
+++ _version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__version__ = "2.0.0"
+__version__ = "3.0.0"
 __author__ = "Amir Zeldes"
 __copyright__ = "Copyright 2015-2019, Amir Zeldes"
 __license__ = "Apache 2.0 License"
