ccc9fa038e35d0a5d4ee7a0b1bbd6df2ed210285
amir-zeldes <amir-zeldes@users.noreply.github.com>
2024-12-17 19:15:58 -0500

    support dialects, context rules and bound group position rules

--- lib/lemmatize.py
+++ lib/lemmatize.py
@@ -2,25 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import io
+import os.path
+import sys
 from collections import defaultdict
 
+# Words after which the lemma of ⲧⲉⲛ should be ⲛⲑⲱⲧⲉⲛ in Bohairic
+boh_ten2 = set(["ⲉⲣⲉ", "ⲁⲣⲉ", "ⲉⲧⲁⲣⲉ", "ⲛⲁⲣⲉ", "ⲙⲡⲉ", "ⲙⲡⲁⲧⲉ", "ϣⲁⲧⲉ", "ϣⲁⲣⲉ", "ⲛⲧⲉ"])
 
 class Lemmatizer:
 
-	def __init__(self, lex_file, no_unknown=True):
+	def __init__(self, lex_file, no_unknown=True, dialect="sahidic"):
 		self.lex = defaultdict(lambda : defaultdict(set))
 		self.no_unknown = no_unknown
+		self.dialect = dialect
 		self.load(lexfile=lex_file)
 
 	def load(self,lexfile):
 		lines = io.open(lexfile,encoding="utf8").read().replace("\r","").split("\n")
 
-		for line in lines:
+		for l, line in enumerate(lines):
 			if "\t" in line:
-				word, pos, lemma = line.split("\t")
+				try:
+					word, pos, lemma = line.split("\t")
+				except ValueError:
+					sys.stderr.write("Error in lexicon on line " + str(l+1) + " of " + os.path.basename(lexfile) + "\n")
+					sys.stderr.write(line + "\n")
+					quit()
 				self.lex[word][pos].add(lemma)
 
-	def lemmatize(self,words_tags):
+	def lemmatize(self,words_tags, bg_positions=None):
 		"""words_tags: string of tab delimited word\tpos per line"""
 
 		lines = words_tags.replace("\r","").strip().split("\n")
@@ -29,7 +39,13 @@ class Lemmatizer:
 
 		output=[]
 		for i, word in enumerate(words):
+			position = bg_positions[i] if bg_positions else None
+			prev_word = words[i-1] if i > 0 else "_"
+			next_word = words[i+1] if i < len(words)-1 else "_"
 			pos = tags[i]
+			next_pos = tags[i+1] if i < len(words)-1 else "_"
+			prev_pos = tags[i-1] if i > 0 else "_"
+
 			if word in self.lex:
 				if pos in self.lex[word]:
 					matches = sorted(list(self.lex[word][pos]))
@@ -39,6 +55,23 @@ class Lemmatizer:
 					else:  # Disambiguate
 						match = matches[0]
 						# TODO: disambiguation code
+						if self.dialect == "bohairic":
+							if word == "ⲧⲉⲛ":
+								if prev_word in boh_ten2:
+									match = "ⲛⲑⲱⲧⲉⲛ"
+								else:
+									match = "ⲁⲛⲟⲛ"
+							elif pos in ["PPOS","PDEM"]:
+								if match in ["ⲡⲁ","ⲫⲁ"]:
+									if next_pos in ["ART","PDEM","PPOS"]:
+										match = "ⲫⲁ"
+									else:
+										match = "ⲡⲁ"
+								elif match in ["ⲡⲁⲓ","ⲫⲁⲓ"]:
+									if next_pos in ["N"]:
+										match = "ⲡⲁⲓ"
+									else:
+										match = "ⲫⲁⲓ"
 
 						output.append(match)
 						continue
@@ -50,7 +83,14 @@ class Lemmatizer:
 						output.append("UNKNOWN")
 			else:
 				if self.no_unknown:
-					output.append(word)
+					lemma = word
+					if pos == "VIMP":
+						if word.startswith("ⲁⲣⲓ"):
+							if self.dialect == "sahidic":
+								lemma = "ⲣ" + word[3:]
+							elif self.dialect == "bohairic":
+								lemma = "ⲉⲣ" + word[3:]
+					output.append(lemma)
 				else:
 					output.append("UNKNOWN")
 		output = ["\t".join([words[i],tags[i],output[i]]) for i in range(len(words))]
